c8191cfa9af354c03ddf55bf670642445b38e6cf
Calvin Bui <c@lvin.email>
2016-01-15 01:39:32 +1100

    New CP API implemented

--- routes/movies.js
+++ routes/movies.js
@@ -1,20 +1,33 @@
 module.exports = function(app, config) {
     var request = require("request");
     var process = require("process");
+    var CouchPotatoAPI = require('couchpotato-api');
+
+    var couchpotato = new CouchPotatoAPI({
+        hostname: config.couchpotato.host,
+        apiKey: config.couchpotato.api,
+        port: config.couchpotato.port,
+        urlBase: config.couchpotato.url_base,
+        ssl: config.couchpotato.ssl,
+        username: config.couchpotato.username,
+        password: config.couchpotato.password
+    });
+
+    function cpApiCall(path, res) {
+        couchpotato.get(path).then(function (result) {
+            res.send(result)
+        }).catch(function (err) {
+            throw new Error("There was a error processing the request: " + err);
+        });
+    }
 
     app.get('/couchpotato_api_comingsoon', function (req, res) {
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // ignore ssl errors
-        function callback(error, response, body) {
-            res.send(JSON.parse(body));
-        }
-        request(config.couchpotato.api_url + config.couchpotato.api + '/media.list?status=active', callback);
+        cpApiCall("media.list?status=active", res);
     });
 
     app.get('/couchpotato_api_downloaded', function (req, res) {
-        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // ignore ssl errors
-        function callback(error, response, body) {
-            res.send(JSON.parse(body));
-        }
-        request(config.couchpotato.api_url + config.couchpotato.api + '/media.list?release_status=available', callback);
+        cpApiCall("/media.list?release_status=available", res);
     });
+
+
 };
